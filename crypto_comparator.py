94df3b5bc86a75a00fb7fdefe016c5bae920b922
Vincent Michler <60316541+vincm1@users.noreply.github.com>
2022-01-12 23:26:49 +0100

    Add files via upload

--- crypto_comparator.py
+++ crypto_comparator.py
@@ -10,9 +10,11 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 #from plotly import graph_obs as go
 
+st.set_page_config(layout="wide")
+
 st.title('Cryptocurrency comparator')
 
-currencies = ("BTC-USD","ETH-USD","USDT-USD","BNB-USD","USDC-USD","SOL-USD","ADA-USD","HEX-USD","XRP-USD","LUNA1-USD","DOT-USD","AVAX-USD","DOGE-USD","SHIB-USD","MATIC-USD","BUSD-USD","CRO-USD","LINK-USD","WBTC-USD","UST-USD","UNI1-USD","ATOM-USD","NEAR-USD","DAI-USD","ALGO-USD","LTC-USD","BCH-USD","FTM-USD","TRX-USD","XLM-USD","ICP-USD","STETH-USD","MANA-USD","VET-USD","HBAR-USD","FTT-USD","BTCB-USD","FIL-USD","SAND-USD","AXS-USD","THETA-USD","ETC-USD","EGLD-USD","HNT-USD","XTZ-USD","ONE1-USD","LEO-USD","XMR-USD","KLAY-USD","MIOTA-USD","GRT1-USD","AAVE-USD","EOS-USD","STX-USD","GALA-USD","CAKE-USD","WBNB-USD","BTT-USD","FLOW-USD","LRC-USD","CRV-USD","RUNE-USD","QNT-USD","KSM-USD","MKR-USD","BSV-USD","ENJ-USD","XEC-USD","FRAX-USD","CVX-USD","ZEC-USD","AMP-USD","CELO-USD","HBTC-USD","KCS-USD","NEO-USD","AR-USD","BAT-USD","OKB-USD","CHZ-USD","KDA-USD","WAVES-USD","SAFEMOON-USD","HT-USD","DASH-USD","NEXO-USD","TUSD-USD","ROSE-USD","MINA-USD","YOUC-USD","YFI-USD","COMP1-USD","RVN-USD","CCXX-USD","HOT1-USD","XDC-USD","XYM-USD","XEM-USD","IOTX-USD","LN-USD")
+currencies = ("BTC-USD","ETH-USD","USDT-USD","BNB-USD","USDC-USD","SOL-USD","ADA-USD","HEX-USD","XRP-USD","LUNA1-USD","DOT-USD","AVAX-USD","DOGE-USD","MATIC-USD","BUSD-USD","CRO-USD","LINK-USD","WBTC-USD","UST-USD","UNI1-USD","ATOM-USD","NEAR-USD","DAI-USD","ALGO-USD","LTC-USD","BCH-USD","FTM-USD","TRX-USD","XLM-USD","ICP-USD","STETH-USD","MANA-USD","VET-USD","HBAR-USD","FTT-USD","BTCB-USD","FIL-USD","SAND-USD","AXS-USD","THETA-USD","ETC-USD","EGLD-USD","HNT-USD","XTZ-USD","ONE1-USD","LEO-USD","XMR-USD","KLAY-USD","MIOTA-USD","GRT1-USD","AAVE-USD","EOS-USD","STX-USD","GALA-USD","CAKE-USD","WBNB-USD","BTT-USD","FLOW-USD","LRC-USD","CRV-USD","RUNE-USD","QNT-USD","KSM-USD","MKR-USD","BSV-USD","ENJ-USD","XEC-USD","FRAX-USD","CVX-USD","ZEC-USD","AMP-USD","CELO-USD","HBTC-USD","KCS-USD","NEO-USD","AR-USD","BAT-USD","OKB-USD","CHZ-USD","KDA-USD","WAVES-USD","SAFEMOON-USD","HT-USD","DASH-USD","NEXO-USD","TUSD-USD","ROSE-USD","MINA-USD","YOUC-USD","YFI-USD","COMP1-USD","RVN-USD","CCXX-USD","HOT1-USD","XDC-USD","XYM-USD","XEM-USD","IOTX-USD","LN-USD")
 
 start_date = st.date_input("Start date", value = datetime.date(datetime(2020, 1, 1)))
 end_date = st.date_input("End date", value = datetime.today())
@@ -39,6 +41,9 @@ st.pyplot(fig3)
 ### Log returns
 log_ret = np.log(data/data.shift(1))
 
+
+### Optimal Portfolio 
+
 def get_ret_vol_sr(weights):
     weights = np.array(weights)
     ret = np.sum(log_ret.mean()*weights*365)
@@ -64,10 +69,19 @@ for i in range(0,len(selected_currencies)):
 
 opt_result = minimize(neg_sharp, init_guess, method="SLSQP", bounds = bounds, constraints=cons)
 
-st.header("Optimum Portfolio weight")
-for i in range(len(selected_currencies)):
-    st.write(selected_currencies[i], opt_result.x[i])
+st.header("Optimal portfolio")
 
+portfolio_weights = pd.DataFrame(data= opt_result.x *100 ,columns=["Portfolio weight"]).set_index(data.columns)
+
+fig1, ax1 = plt.subplots()
+ax1.pie(portfolio_weights["Portfolio weight"], labels=portfolio_weights.index, autopct='%1.1f%%', startangle=90) 
+# Equal aspect ratio ensures that pie is drawn as a circle.
+st.pyplot(fig1)
+
+return_vol_sharpe = pd.DataFrame(get_ret_vol_sr(opt_result.x), index=["Return","Volatility","Sharp-ratio"],columns=["Portfolio KPIs:"])
+st.write(return_vol_sharpe)
+
+# ML Algos 
 
 ### Prophet 
 # n_years = st.slider("Years of prediction:",1,3)
