6c4f2bb01016422db4373efa786aeb48bf2471e6
Vincent Michler <60316541+vincm1@users.noreply.github.com>
2022-01-12 22:34:21 +0100

    Add files via upload

--- crypto_comparator.py
+++ crypto_comparator.py
@@ -3,43 +3,74 @@ import yfinance as yf
 from datetime import datetime, date
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.optimize import minimize
 from prophet import Prophet
 from prophet.plot import plot_plotly
 #from plotly import graph_obs as go
 
-
-import pandas as pd
-import numpy as np
-
-START = "2015-01-01"
-TODAY = "2021-12-31"
-
 st.title('Cryptocurrency comparator')
 
-
 currencies = ("BTC-USD","ETH-USD","USDT-USD","BNB-USD","USDC-USD","SOL-USD","ADA-USD","HEX-USD","XRP-USD","LUNA1-USD","DOT-USD","AVAX-USD","DOGE-USD","SHIB-USD","MATIC-USD","BUSD-USD","CRO-USD","LINK-USD","WBTC-USD","UST-USD","UNI1-USD","ATOM-USD","NEAR-USD","DAI-USD","ALGO-USD","LTC-USD","BCH-USD","FTM-USD","TRX-USD","XLM-USD","ICP-USD","STETH-USD","MANA-USD","VET-USD","HBAR-USD","FTT-USD","BTCB-USD","FIL-USD","SAND-USD","AXS-USD","THETA-USD","ETC-USD","EGLD-USD","HNT-USD","XTZ-USD","ONE1-USD","LEO-USD","XMR-USD","KLAY-USD","MIOTA-USD","GRT1-USD","AAVE-USD","EOS-USD","STX-USD","GALA-USD","CAKE-USD","WBNB-USD","BTT-USD","FLOW-USD","LRC-USD","CRV-USD","RUNE-USD","QNT-USD","KSM-USD","MKR-USD","BSV-USD","ENJ-USD","XEC-USD","FRAX-USD","CVX-USD","ZEC-USD","AMP-USD","CELO-USD","HBTC-USD","KCS-USD","NEO-USD","AR-USD","BAT-USD","OKB-USD","CHZ-USD","KDA-USD","WAVES-USD","SAFEMOON-USD","HT-USD","DASH-USD","NEXO-USD","TUSD-USD","ROSE-USD","MINA-USD","YOUC-USD","YFI-USD","COMP1-USD","RVN-USD","CCXX-USD","HOT1-USD","XDC-USD","XYM-USD","XEM-USD","IOTX-USD","LN-USD")
-selected_currencies = st.selectbox("Select currencies for comparison", currencies)
 
-n_years = st.slider("Years of prediction:",1,3)
-period = n_years * 365 
+start_date = st.date_input("Start date", value = datetime.date(datetime(2020, 1, 1)))
+end_date = st.date_input("End date", value = datetime.today())
+
+selected_currencies = st.multiselect("Select currencies for comparison", currencies, default = ["BTC-USD"])
 
 @st.cache
+
 def load_data(ticker):
-    data = yf.download(ticker,START,TODAY)
-    data.reset_index(inplace=True)
+    data = yf.download(ticker,start_date,end_date)
+    data = data["Adj Close"]
     return data
 
 data = load_data(selected_currencies)
 
-st.subheader("Raw data")
-st.write(data.tail())
+st.header("Adj Close Prices")
+st.line_chart(data)
 
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data["Date"],y=data["Open"],name="Currency open"))
-    fig.add_trace(go.Scatter(x=data["Date"],y=data["Close"],name="Currency close"))
-    fig.layout.update(title_text="Time Series Data",xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+st.header("Correlation")
+fig3 = plt.figure()
+sns.heatmap(data.corr(),annot=True)
+st.pyplot(fig3)
 
-plot_raw_data
+### Log returns
+log_ret = np.log(data/data.shift(1))
+
+def get_ret_vol_sr(weights):
+    weights = np.array(weights)
+    ret = np.sum(log_ret.mean()*weights*365)
+    vola = np.sqrt(np.dot(weights.T,np.dot(log_ret.cov()*365, weights)))
+    sharp_ratio = ret / vola 
+    return np.array([ret,vola,sharp_ratio])
+
+def neg_sharp(weights):
+    return get_ret_vol_sr(weights)[2]*-1
+
+def check_sum(weights):
+    return np.sum(weights)-1
+
+cons = ({"type":"eq","fun":check_sum})
+
+init_guess = [1/len(selected_currencies) for i in selected_currencies]
+
+zero_one = ((0,1),)
+bounds = ()
+
+for i in range(0,len(selected_currencies)):
+    bounds = bounds + zero_one
+
+opt_result = minimize(neg_sharp, init_guess, method="SLSQP", bounds = bounds, constraints=cons)
+
+st.header("Optimum Portfolio weight")
+for i in range(len(selected_currencies)):
+    st.write(selected_currencies[i], opt_result.x[i])
+
+
+### Prophet 
+# n_years = st.slider("Years of prediction:",1,3)
+# period = n_years * 365 
+
+### XG Boost / Random Forrest 
